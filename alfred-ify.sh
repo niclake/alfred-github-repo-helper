1f473938781d41b440f8543b3fc2d546c34e8afa
Nic Lake <niclake13@gmail.com>
2023-01-27 20:09:04 -0600

    Add parentheses to if ... end

--- alfred-ify.sh
+++ alfred-ify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat ./user-repos.json | $(brew --prefix)/bin/jq -s 'map(.[]|{ arg: [.html_url, .name], uid: .id, title: .name, subtitle: if .description then .description else .url end }) | { items: . }' > ./alfred-user-repos.json
+cat ./user-repos.json | $(brew --prefix)/bin/jq -s 'map(.[]|{ arg: [.html_url, .name], uid: .id, title: .name, subtitle: (if .description then .description else .url end) }) | { items: . }' > ./alfred-user-repos.json
